c9f764e0fe4b747c9810ace28039e0133fa06e5f
Das <puja.i.das@accenture.com>
2025-01-15 15:38:44 +0530

    updated the path in main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // main.js (main-project-repo)
-const { greet } = require('./libs/submodule-repo/utils');  // Importing greet from submodule
+const { greet } = require('./demo/submodule-repo/utils');  // Importing greet from submodule
 
 console.log(greet('Alice'));  // This will use the submodule's function
